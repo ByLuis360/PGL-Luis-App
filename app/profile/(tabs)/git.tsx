d2cb9ede3caae367a3310e3a6eca7969bad5ad37
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-11-12 23:37:11 +0000

    feat: código qr a mi repositorio

--- app/profile/(tabs)/git.tsx
+++ app/profile/(tabs)/git.tsx
@@ -1,14 +1,33 @@
 import { Tabs } from 'expo-router'
 import React from 'react'
 import { StyleSheet ,Text, View } from 'react-native'
+import QRCode from 'react-native-qrcode-svg'
 
 
 export const gitPage = () => {
     return (
-     <Text>git</Text>
-    )
+        <View style={styles.qrContainer}>
+              <Text style={{ backgroundColor: "white" }}>
+                My repository
+              </Text>
+              <QRCode
+                value="https://github.com/ByLuis360"
+                logo={require("../../../assets/images/mondongo.jpg")}
+                logoSize={10}
+                logoBackgroundColor="transparent"
+              />
+            </View>
+      )
 }
 
 export default gitPage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    qrContainer: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 12,
+        marginTop: 12,
+      },
+})
