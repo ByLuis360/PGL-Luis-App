7ace00289372ecaff60b81d57f041dfc7fde72e9
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-11-12 12:04:52 +0000

    feat: lista de hobbies

---
+++ components/Cards.tsx
@@ -0,0 +1,28 @@
+import { CardProp } from "./Card";
+
+export const cards: CardProp[] = [
+  {
+    paragraph: "Quedarme en casa",
+  },
+  {
+    paragraph: "Jugar ajedrez",
+  },
+  {
+    paragraph: "Jugar al billar",
+  },
+  {
+    paragraph: "A veces jugar furbo",
+  },
+  {
+    paragraph: "Tardecita de play",
+  },
+  {
+    paragraph: "Salir a comer",
+  },
+  {
+    paragraph: "Dragon Ball",
+  },
+  {
+    paragraph: "Ver pelis",
+  },
+];
