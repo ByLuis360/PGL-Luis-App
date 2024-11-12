1f897b4b1554ec5f19a6b3afd0007d56db5d1579
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-11-12 11:49:11 +0000

    feat: header por defecto

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,47 @@
 import React from 'react'
-import { StyleSheet ,Text, View } from 'react-native'
+import { Image, StyleSheet ,Text, View } from 'react-native'
 
 export const Header = () => {
-    return (
-      <View>
-        <Text> Esto es el header </Text>
+  return (
+    <View style={styles.descriptionContainer}>
+      <Image
+        style={styles.avatar}
+        source={require("../../../assets/arbol.png")}
+      ></Image>
+      <View style={styles.paragraphContainer}>
+        <Text style={styles.title}>Descripción sobre mí!</Text>
+        <Text>
+          Soy un alumno un poco bastante vago pero que le gusta lo que hace y sobretodo y
+          más importante, soy del Barça xddd
+        </Text>
       </View>
-    )
+    </View>
+  );
   }
 
 export default Header
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  avatar: {
+    marginTop: 20,
+    height: 100,
+    width: 100,
+    borderRadius: 100,
+  },
+  descriptionContainer: {
+    alignItems: "center",
+  },
+  paragraphContainer: {
+    margin: 10,
+    marginBottom: 60,
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 10,
+    width: "70%",
+  },
+  title: {
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: 20,
+  },
+});
