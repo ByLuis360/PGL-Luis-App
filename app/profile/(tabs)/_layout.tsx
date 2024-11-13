5b0cd705ed401947578cdbb456181be466da0c83
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-11-13 00:30:40 +0000

    feat: estilo de los tabs

--- app/profile/(tabs)/_layout.tsx
+++ app/profile/(tabs)/_layout.tsx
@@ -1,20 +1,33 @@
-import { Tabs } from 'expo-router'
-import React from 'react'
-import { StyleSheet ,Text, View } from 'react-native'
-import Header from '../../../components/Header'
+import { Tabs } from "expo-router";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "../../../components/Header";
+import Entypo from "@expo/vector-icons/Entypo";
 
 export const tabsLayout = () => {
-    return (
-      <Tabs screenOptions={{
-        tabBarActiveTintColor: "red",
-        header: () => <Header/>
-      }}>
-        <Tabs.Screen name='hobbies'/>
-        <Tabs.Screen name='git'/>
-      </Tabs>
-    )
-}
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "blue",
+        tabBarActiveBackgroundColor: "red",
+        header: () => <Header />,
+      }}
+    >
+      <Tabs.Screen
+        name="hobbies"
+        options={{
+          title: "Hobbies",
+          tabBarIcon: () => <Entypo name="video" />,
+        }}
+      />
+      <Tabs.Screen name="git" options={{
+        title: "Git",
+        tabBarIcon: () => <Entypo name="github" />
+      }} />
+    </Tabs>
+  );
+};
 
-export default tabsLayout
+export default tabsLayout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
